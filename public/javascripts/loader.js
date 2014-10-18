531ea4166f5f599fc7f2ac147c625fbaa7cddd21
Kirill Mangutov <kmangutov@gmail.com>
2014-10-18 18:50:42 -0400

    loading more data from linkedin

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -2,45 +2,27 @@
 function onLinkedInAuth() {
 	console.log("LinkedIn API framework initialized!");
 	//reference: https://developer.linkedin.com/documents/javascript-api-tutorial
-	var fields =
-	["id",
-	"first-name",
-	"last-name",
-	"maiden-name",
-	"formatted-name",
-	"phonetic-first-name",
-	"phonetic-last-name",
-	"formatted-phonetic-name",
-	"headline",
-	"location:(name)",
-	"location:(country:code))",
-	"industry",
-	//"distance",
-	//"relation-to-viewer:(distance)",
-	//"relation-to-viewer(related-connections)",
-	"current-share",
-	//"num-connections",
-	//"num-connections-capped",
-	"summary",
-	"specialties",
-	"positions",
-	"picture-url",
-	"site-standard-profile-request",
-	"api-standard-profile-request:(url)",
-	"api-standard-profile-request:(headers)",
-	"public-profile-url" ];
-	IN.API.Profile("me").fields(fields)result(displayProfiles); //argument for result is callback function
-}
+	var fields = [
+		"first-name",
+		"last-name",
+		"headline",
+		"location:(name)",
+		"industry",
+		"num-connections",
+		"summary",
+		"specialties",
+		"positions",
+		"picture-url",
+		"public-profile-url"
+	];
 
+	IN.API.Profile("me").fields(fields).result(displayProfiles);
+}
+ 
 function displayProfiles(profiles)
 {
 	member = profiles.values[0];
-	//	 document.getElementById("profiles").innerHTML = 
-	//"<p id=\"" + member.id + "\">Hello " +  member.firstName + " " + member.lastName + "</p>";
   	enumerate_profile_properties(member);
-
-//  	document.getElementById("profiles").innerHTML = 
-//  		member;
 }
 
 function enumerate_profile_properties(member)
